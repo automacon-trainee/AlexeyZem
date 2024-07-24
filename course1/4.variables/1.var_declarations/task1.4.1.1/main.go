f7b5d73eb25b3ca270d9f3feec8d5545c928bb38
Alexeyzem <Alex.zem2015@mail.ru>
2024-07-24 09:54:00 +0300

    1.var_declarations done

---
+++ course1/4.variables/1.var_declarations/task1.4.1.1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numA int
+	var numB int
+	var numC int
+	var numD int
+	var numE int
+	var numF int
+	var numG int
+	var numH int
+	numA = 0
+	numB = 1
+	numC = 1
+	numD = 2
+	numE = 3
+	numF = 5
+	numG = 8
+	numH = 13
+	fmt.Println(numA, numB, numC, numD, numE, numF, numG, numH)
+}
