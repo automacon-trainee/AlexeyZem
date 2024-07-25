f03d9e0aa66e917bb47b84851a0ab3853fc5d0f9
Alexeyzem <Alex.zem2015@mail.ru>
2024-07-25 11:55:50 +0300

    1.constant_dec done

---
+++ course1/6.constants/1.constant_dec/task1.6.1.1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	Monday    = "Понедельни"
+	Tuesday   = "Вторник"
+	Wednesday = "Среда"
+	Thursday  = "Четверг"
+	Friday    = "Пятница"
+	Saturday  = "Суббота"
+	Sunday    = "Воскресенье"
+)
+
+func main() {
+	fmt.Println(Monday)
+	fmt.Println(Tuesday)
+	fmt.Println(Wednesday)
+	fmt.Println(Thursday)
+	fmt.Println(Friday)
+	fmt.Println(Saturday)
+	fmt.Println(Sunday)
+}
