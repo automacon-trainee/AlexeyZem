49b3243536a0b27bc0083a7787d36adfe0f7cfc3
Alexeyzem <Alex.zem2015@mail.ru>
2024-07-30 15:42:59 +0300

    5.package_version done

---
+++ course1/12.package/5.package_version/task1.12.5.1/go.mod
@@ -0,0 +1,5 @@
+module student.vkusvill.ru/AlexeyZem/go-course/golang_study_ak/course1/12.package/5.package_version/task1.12.5.1
+
+go 1.21
+
+require github.com/AlexeyZem/mymath v1.0.0 //indirect
---
+++ course1/12.package/5.package_version/task1.12.5.1/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/AlexeyZem/mymath"
+)
+
+func main() {
+	x := 4.0
+	y := 2.0
+	fmt.Println("Square root of", x, "is", mymath.Sqrt(x))
+	fmt.Println("Ceil of ", x, "is", mymath.Ceil(x))
+	fmt.Println("Floor of ", x, "is", mymath.Floor(x))
+	fmt.Println("Power of", x, "to", y, "is", mymath.Pow(x, y))
+	fmt.Println("Max of", x, "and", y, "is", mymath.Max(x, y))
+	fmt.Println("Min of", x, "and", y, "is", mymath.Min(x, y))
+	fmt.Println("Abs of", -x, "is", mymath.Abs(x))
+}
