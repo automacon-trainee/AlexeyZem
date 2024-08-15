8a44f1ab555e2498ab67075fc4c1052c104e8268
Alexeyzem <Alex.zem2015@mail.ru>
2024-08-15 19:26:45 +0300

    fix task1.7.2.1

--- course1/7.primitives/2.types_int/task1.7.2.1/main.go
+++ course1/7.primitives/2.types_int/task1.7.2.1/main.go
@@ -5,24 +5,24 @@ import (
 	"log"
 )
 
-func calculate(a, b int) (sum, difference, product, quotient, remainder int) {
+func calculate(a, b int) (sum, difference, multiplication, quotient, remainder int) {
 	sum = a + b
 	difference = a - b
-	product = a * b
+	multiplication = a * b
 	quotient = a / b
 	remainder = a % b
-	return sum, difference, product, quotient, remainder
+	return
 }
 
 func main() {
-	var a, b, sum, difference, product, quotient, remainder int
+	var a, b int
 	_, err := fmt.Scanln(&a, &b)
 	if err != nil {
 		log.Println("Wrong data:", err)
 		return
 	}
 
-	sum, difference, product, quotient, remainder = calculate(a, b)
+	sum, difference, product, quotient, remainder := calculate(a, b)
 	fmt.Printf("a = %d, b = %d\nsum = %d, difference = %d, product = %d, quotient = %d, remainder = %d\n",
 		a, b, sum, difference, product, quotient, remainder)
 }
