b01dd707143fb59f756fabe2391596da0b193109
Alexeyzem <Alex.zem2015@mail.ru>
2024-08-15 15:17:15 +0300

    fix task2.4.5.2

--- course2/4.algo_datastruct/5.datastruct_queue/task2.4.5.2/main.go
+++ course2/4.algo_datastruct/5.datastruct_queue/task2.4.5.2/main.go
@@ -10,15 +10,20 @@ type BrowseHistory struct {
 
 func (h *BrowseHistory) Visit(url string) {
 	h.stack = append(h.stack, url)
+	fmt.Println("Посещение", url)
 }
 
 func (h *BrowseHistory) Back() {
 	if len(h.stack) > 0 {
 		h.stack = h.stack[:len(h.stack)-1]
+		fmt.Println("Возврат к", h.stack[len(h.stack)-1])
+	} else {
+		fmt.Println("Нет больше истории для возврата")
 	}
 }
 
 func (h *BrowseHistory) PrintHistory() {
+	fmt.Println("История браузера:")
 	for i := len(h.stack) - 1; i >= 0; i-- {
 		fmt.Println(h.stack[i])
 	}
