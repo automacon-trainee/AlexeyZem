333a910f31444775b31e92c455c9e22841da9dc8
Alexeyzem <Alex.zem2015@mail.ru>
2024-08-20 16:06:03 +0300

    task 3.2.3.1 done

---
+++ course3/2.server/3.server_http_router/task3.2.3.1/main_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNewRouter(t *testing.T) {
+	r := NewRouter()
+	if r == nil {
+		t.Errorf("NewRouter() returned nil")
+	}
+}
+
+func TestFirstHandler(t *testing.T) {
+	r := NewRouter()
+	req, _ := http.NewRequest("GET", "/1", nil)
+	resp := httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+	if resp.Code != http.StatusOK {
+		t.Errorf("TestFirstHandler() returned %v", resp.Code)
+	}
+	if resp.Body.String() != "Hello World" {
+		t.Errorf("TestFirstHandler() returned %v", resp.Body.String())
+	}
+}
+
+func TestSecondHandler(t *testing.T) {
+	r := NewRouter()
+	req, _ := http.NewRequest("GET", "/2", nil)
+	resp := httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+	if resp.Code != http.StatusOK {
+		t.Errorf("TestFirstHandler() returned %v", resp.Code)
+	}
+	if resp.Body.String() != "Hello World 2" {
+		t.Errorf("TestFirstHandler() returned %v", resp.Body.String())
+	}
+}
+
+func TestThirdHandler(t *testing.T) {
+	r := NewRouter()
+	req, _ := http.NewRequest("GET", "/3", nil)
+	resp := httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+	if resp.Code != http.StatusOK {
+		t.Errorf("TestFirstHandler() returned %v", resp.Code)
+	}
+	if resp.Body.String() != "Hello World 3" {
+		t.Errorf("TestFirstHandler() returned %v", resp.Body.String())
+	}
+}
+
+func TestDefaultHandler(t *testing.T) {
+	r := NewRouter()
+	req, _ := http.NewRequest("GET", "/", nil)
+	resp := httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+	if resp.Code != http.StatusNotFound {
+		t.Errorf("TestFirstHandler() returned %v", resp.Code)
+	}
+	if resp.Body.String() != "Not Found" {
+		t.Errorf("TestFirstHandler() returned %v", resp.Body.String())
+	}
+}
