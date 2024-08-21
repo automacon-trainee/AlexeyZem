779b4be5bcc1a6c0d88a90a63cd1cb3966babfe0
Alexeyzem <Alex.zem2015@mail.ru>
2024-08-21 16:22:17 +0300

    task3.2.7.1 no docs

---
+++ course3/2.server/7.authorization/task3.2.7.1/go.mod
@@ -0,0 +1,18 @@
+module auth
+
+go 1.21
+
+require github.com/go-chi/chi v1.5.5
+
+require (
+	github.com/go-chi/jwtauth v1.2.0 // indirect
+	github.com/goccy/go-json v0.3.5 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.7 // indirect
+	github.com/lestrrat-go/httpcc v1.0.0 // indirect
+	github.com/lestrrat-go/iter v1.0.0 // indirect
+	github.com/lestrrat-go/jwx v1.1.0 // indirect
+	github.com/lestrrat-go/option v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+)
---
+++ course3/2.server/7.authorization/task3.2.7.1/internal/server.go
@@ -0,0 +1,39 @@
+package internal
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
+)
+
+func NewRouter() *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Group(func(router chi.Router) {
+		router.Use(jwtauth.Verifier(auth))
+		router.Use(jwtauth.Authenticator)
+		router.Post("/api/address/search", handlerSearch)
+		router.Post("/api/address/geocode", handlerGeocode)
+	})
+	router.Post("/api/login", LoginHandler)
+	router.Post("/api/register", RegisterHandler)
+	router.NotFound(handlerNot)
+	return router
+}
+
+func handlerNot(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	_, _ = w.Write([]byte("not found"))
+}
+
+type Server struct{}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s *Server) Start() error {
+	return http.ListenAndServe(":8080", NewRouter())
+}
