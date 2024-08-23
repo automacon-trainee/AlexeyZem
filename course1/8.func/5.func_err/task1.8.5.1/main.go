5e50f8bb7838d5d640307d9a1324d676bbceac78
Alexeyzem <Alex.zem2015@mail.ru>
2024-08-23 11:12:01 +0300

    fix task1.8.5.1

--- course1/8.func/5.func_err/task1.8.5.1/main.go
+++ course1/8.func/5.func_err/task1.8.5.1/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"math"
 	"strconv"
 )
 
 func CalculatePercentageChange(initalValue, finalValue string) (float64, error) {
-	defer func() {
-		if err := recover(); err != nil {
-			fmt.Println("Zero value")
-		}
-	}()
+	if initalValue == "0" {
+		return 0.0, errors.New("inital value is zero")
+	}
 	finValue, err := strconv.ParseFloat(finalValue, 64)
 	if err != nil {
 		return 0, err
@@ -19,16 +19,15 @@ func CalculatePercentageChange(initalValue, finalValue string) (float64, error) 
 	if err != nil {
 		return 0, err
 	}
+
 	fromNumToPercent := 100.0
-	if finalValue > initalValue {
-		return (finValue/initValue - 1) * fromNumToPercent, nil
-	}
-	return (finValue/initValue - 1) * -fromNumToPercent, nil
+
+	return math.Abs((finValue/initValue - 1) * fromNumToPercent), nil
 }
 
 func main() {
 	initialValue := "10.0"
-	finalValue := "25"
+	finalValue := "1"
 	res, err := CalculatePercentageChange(initialValue, finalValue)
 	if err != nil {
 		fmt.Println("err:", err)
