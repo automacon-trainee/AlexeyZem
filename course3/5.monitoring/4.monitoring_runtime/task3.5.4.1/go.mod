2c163d0d87f54a9db8f9e43755ad64e8895a51e7
Alexeyzem <Alex.zem2015@mail.ru>
2024-08-29 10:29:48 +0300

    task 3.5.4.1 done

---
+++ course3/5.monitoring/4.monitoring_runtime/task3.5.4.1/go.mod
@@ -0,0 +1,5 @@
+module student.vkusvill.ru/AlexeyZem/go-course/golang_study_ak/course3/5.monitoring/4.monitoring_runtime/task3.5.4.1
+
+go 1.21
+
+require golang.org/x/sync v0.8.0
---
+++ course3/5.monitoring/4.monitoring_runtime/task3.5.4.1/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"runtime"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func CalculatePercentageChange(initalValue, finalValue int) (float64, error) {
+	if finalValue == 0 {
+		return 0.0, errors.New("cannot calculate percentage")
+	}
+
+	fromNumToPercent := 100.0
+
+	return (float64(finalValue)/float64(initalValue) - 1) * fromNumToPercent, nil
+}
+
+func MonitorGoroutine(prevGoroutines int) {
+	for {
+		fmt.Println("Количество горутин: ", prevGoroutines)
+		curGoroutines := runtime.NumGoroutine()
+		percent, err := CalculatePercentageChange(prevGoroutines, curGoroutines)
+		if err != nil {
+			fmt.Printf("error in calculating percentage: %v\n", err)
+		}
+		if percent >= 20 {
+			fmt.Println("Предупреждение: количество горутин увеличилось более чем на 20%")
+		}
+		if percent <= -20 {
+			fmt.Println("Предупреждение: количество горутин уменьшилось более чем на 20%")
+		}
+		prevGoroutines = curGoroutines
+		time.Sleep(300 * time.Millisecond)
+	}
+}
+
+func main() {
+	g, _ := errgroup.WithContext(context.Background())
+	go MonitorGoroutine(runtime.NumGoroutine())
+
+	for i := 0; i < 100; i++ {
+		g.Go(func() error {
+			time.Sleep(5 * time.Second)
+			return nil
+		})
+		time.Sleep(80 * time.Millisecond)
+	}
+	if err := g.Wait(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+}
