899e3328c65da0d41e2eb3f1da64a7b94b686c1f
Alexeyzem <Alex.zem2015@mail.ru>
2024-09-18 16:18:01 +0300

    done api gateway(proxy)

---
+++ course4/2.microservices/3.gateway/task4.2.3.1/internal/controller/middleware.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"metrics/internal/API/gRPCAuth"
+)
+
+type Middleware struct {
+	auth gRPCAuth.AuthServiceClient
+}
+
+func NewMiddleware(auth gRPCAuth.AuthServiceClient) *Middleware {
+	return &Middleware{auth: auth}
+}
+
+func (m *Middleware) AuthVerify(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenStr := r.Header.Get("Authorization")
+		tokenStr = strings.TrimPrefix(tokenStr, "Bearer ")
+		token := gRPCAuth.Token{Token: tokenStr}
+		_, err := m.auth.VerifyToken(context.Background(), &token)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			_, _ = w.Write([]byte(err.Error()))
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
